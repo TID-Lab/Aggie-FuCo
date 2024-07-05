536cdc5edd1653f0331eeeb070a0fde73ad01939
last night <fujizhang2525@gmail.com>
2024-07-05 19:17:35 -0400

    remove comma-number and replace with vanilla js

--- src/components/AggiePagination.tsx
+++ src/components/AggiePagination.tsx
@@ -1,144 +1,201 @@
-import {Pagination, Placeholder, Stack} from "react-bootstrap";
+import { Pagination, Placeholder, Stack } from "react-bootstrap";
 import React from "react";
-import {useSearchParams} from "react-router-dom";
-const commaNumber = require('comma-number')
+import { useSearchParams } from "react-router-dom";
 
 interface IProps {
-  goToPage: (pageNum: number) => void,
-  total: number,
-  itemsPerPage: number,
-  size?: "sm" | "lg",
-  variant?: "modal" | "page",
-  page?: number | null
+  goToPage: (pageNum: number) => void;
+  total: number;
+  itemsPerPage: number;
+  size?: "sm" | "lg";
+  variant?: "modal" | "page";
+  page?: number | null;
 }
 
 const AggiePagination = (props: IProps) => {
   const [searchParams, setSearchParams] = useSearchParams();
+  function formatNumber(number: number): string {
+    return number.toLocaleString();
+  }
   if (props.variant === "modal") {
     const pageNum = props.page || 0;
     return (
-        <Stack direction={"horizontal"} gap={2} className={"justify-content-end"}>
-          {(props.itemsPerPage > props.total) &&
-              <small className={"me-2"}>
-                {commaNumber(pageNum * props.itemsPerPage + 1)} - {commaNumber(props.total)}
-                {" "} of {commaNumber(props.total)}
-              </small>
-          }
-          {(props.total > props.itemsPerPage) &&
-              <small className={"me-2"}>
-                {commaNumber(pageNum * props.itemsPerPage + 1)} - {commaNumber(pageNum * props.itemsPerPage + props.itemsPerPage + 1)}
-                {" "} of {commaNumber(props.total)}
-              </small>
-          }
-          <Pagination className={"mb-0"} size={props.size ? props.size : undefined}>
-            {pageNum === 0 &&
-                <>
-                  <Pagination.First disabled/>
-                  <Pagination.Prev disabled aria-disabled="true"/>
-                </>
-            }
-            {pageNum > 0 &&
-                <>
-                  <Pagination.First onClick={()=>props.goToPage(0)}/>
-                  <Pagination.Prev onClick={()=>props.goToPage( pageNum - 1)}/>
-                  <Pagination.Item onClick={()=>props.goToPage( pageNum - 1)}>
-                    {pageNum - 1}
-                  </Pagination.Item>
-                </>
-            }
-            <Pagination.Item disabled aria-disabled="true">{pageNum}</Pagination.Item>
-            {pageNum + 1 < (props.total/props.itemsPerPage) &&
-                <>
-                  <Pagination.Item onClick={()=>props.goToPage(pageNum + 1)}>
-                    {pageNum + 1}
-                  </Pagination.Item>
-                  <Pagination.Next onClick={()=>props.goToPage(pageNum + 1)}/>
-                  <Pagination.Last onClick={()=>props.goToPage(Math.ceil(props.total/props.itemsPerPage - 1))}/>
-                </>
-            }
-            {pageNum >= (props.total/props.itemsPerPage) &&
-                <>
-                  <Pagination.Next disabled/>
-                  <Pagination.Last disabled/>
-                </>
-            }
-          </Pagination>
-        </Stack>
-    )
+      <Stack direction={"horizontal"} gap={2} className={"justify-content-end"}>
+        {props.itemsPerPage > props.total && (
+          <small className={"me-2"}>
+            {formatNumber(pageNum * props.itemsPerPage + 1)} -{" "}
+            {formatNumber(props.total)} of {formatNumber(props.total)}
+          </small>
+        )}
+        {props.total > props.itemsPerPage && (
+          <small className={"me-2"}>
+            {formatNumber(pageNum * props.itemsPerPage + 1)} -{" "}
+            {formatNumber(
+              pageNum * props.itemsPerPage + props.itemsPerPage + 1
+            )}{" "}
+            of {formatNumber(props.total)}
+          </small>
+        )}
+        <Pagination
+          className={"mb-0"}
+          size={props.size ? props.size : undefined}
+        >
+          {pageNum === 0 && (
+            <>
+              <Pagination.First disabled />
+              <Pagination.Prev disabled aria-disabled='true' />
+            </>
+          )}
+          {pageNum > 0 && (
+            <>
+              <Pagination.First onClick={() => props.goToPage(0)} />
+              <Pagination.Prev onClick={() => props.goToPage(pageNum - 1)} />
+              <Pagination.Item onClick={() => props.goToPage(pageNum - 1)}>
+                {pageNum - 1}
+              </Pagination.Item>
+            </>
+          )}
+          <Pagination.Item disabled aria-disabled='true'>
+            {pageNum}
+          </Pagination.Item>
+          {pageNum + 1 < props.total / props.itemsPerPage && (
+            <>
+              <Pagination.Item onClick={() => props.goToPage(pageNum + 1)}>
+                {pageNum + 1}
+              </Pagination.Item>
+              <Pagination.Next onClick={() => props.goToPage(pageNum + 1)} />
+              <Pagination.Last
+                onClick={() =>
+                  props.goToPage(
+                    Math.ceil(props.total / props.itemsPerPage - 1)
+                  )
+                }
+              />
+            </>
+          )}
+          {pageNum >= props.total / props.itemsPerPage && (
+            <>
+              <Pagination.Next disabled />
+              <Pagination.Last disabled />
+            </>
+          )}
+        </Pagination>
+      </Stack>
+    );
   } else {
     if (props.total === 0) {
-      return (
-          <></>
-      )
+      return <></>;
     } else {
       return (
-          <Stack direction={"horizontal"} gap={2} className={"justify-content-end"}>
-            {(props.itemsPerPage > props.total) &&
-                <small className={"me-2"}>
-                  {commaNumber(Number(searchParams.get('page')) * props.itemsPerPage + 1)} - {commaNumber(props.total)}
-                  {" "} of {commaNumber(props.total)}
-                </small>
-            }
-            {(props.total > props.itemsPerPage) &&
-                <small className={"me-2"}>
-                  {commaNumber(Number(searchParams.get('page')) * props.itemsPerPage + 1)} - {commaNumber(Number(searchParams.get('page')) * props.itemsPerPage + props.itemsPerPage + 1)}
-                  {" "} of {commaNumber(props.total)}
-                </small>
-            }
-            <Pagination className={"mb-0"} size={props.size ? props.size : undefined}>
-              {Number(searchParams.get('page')) === 0 &&
-                  <>
-                    <Pagination.First disabled/>
-                    <Pagination.Prev disabled aria-disabled="true"/>
-                  </>
-              }
-              {Number(searchParams.get('page')) > 0 &&
-                  <>
-                    <Pagination.First onClick={()=>props.goToPage(0)}/>
-                    <Pagination.Prev onClick={()=>props.goToPage(Number(searchParams.get('page')) - 1)}/>
-                    <Pagination.Item onClick={()=>props.goToPage(Number(searchParams.get('page')) - 1)}>
-                      {Number(searchParams.get('page')) - 1}
-                    </Pagination.Item>
-                  </>
-              }
-              <Pagination.Item disabled aria-disabled="true">{Number(searchParams.get('page'))}</Pagination.Item>
-              {Number(searchParams.get('page')) + 1 < (props.total/props.itemsPerPage) &&
-                  <>
-                    <Pagination.Item onClick={()=>props.goToPage(Number(searchParams.get('page')) + 1)}>
-                      {Number(searchParams.get('page')) + 1}
-                    </Pagination.Item>
-                    <Pagination.Next onClick={()=>props.goToPage(Number(searchParams.get('page')) + 1)}/>
-                    <Pagination.Last onClick={()=>props.goToPage(Math.ceil(props.total/props.itemsPerPage - 1))}/>
-                  </>
-              }
-              {Number(searchParams.get('page')) + 1 >= (props.total/props.itemsPerPage) &&
-                  <>
-                    <Pagination.Next disabled/>
-                    <Pagination.Last disabled/>
-                  </>
-              }
-            </Pagination>
-          </Stack>
-      )
+        <Stack
+          direction={"horizontal"}
+          gap={2}
+          className={"justify-content-end"}
+        >
+          {props.itemsPerPage > props.total && (
+            <small className={"me-2"}>
+              {formatNumber(
+                Number(searchParams.get("page")) * props.itemsPerPage + 1
+              )}{" "}
+              - {formatNumber(props.total)} of {formatNumber(props.total)}
+            </small>
+          )}
+          {props.total > props.itemsPerPage && (
+            <small className={"me-2"}>
+              {formatNumber(
+                Number(searchParams.get("page")) * props.itemsPerPage + 1
+              )}{" "}
+              -{" "}
+              {formatNumber(
+                Number(searchParams.get("page")) * props.itemsPerPage +
+                  props.itemsPerPage +
+                  1
+              )}{" "}
+              of {formatNumber(props.total)}
+            </small>
+          )}
+          <Pagination
+            className={"mb-0"}
+            size={props.size ? props.size : undefined}
+          >
+            {Number(searchParams.get("page")) === 0 && (
+              <>
+                <Pagination.First disabled />
+                <Pagination.Prev disabled aria-disabled='true' />
+              </>
+            )}
+            {Number(searchParams.get("page")) > 0 && (
+              <>
+                <Pagination.First onClick={() => props.goToPage(0)} />
+                <Pagination.Prev
+                  onClick={() =>
+                    props.goToPage(Number(searchParams.get("page")) - 1)
+                  }
+                />
+                <Pagination.Item
+                  onClick={() =>
+                    props.goToPage(Number(searchParams.get("page")) - 1)
+                  }
+                >
+                  {Number(searchParams.get("page")) - 1}
+                </Pagination.Item>
+              </>
+            )}
+            <Pagination.Item disabled aria-disabled='true'>
+              {Number(searchParams.get("page"))}
+            </Pagination.Item>
+            {Number(searchParams.get("page")) + 1 <
+              props.total / props.itemsPerPage && (
+              <>
+                <Pagination.Item
+                  onClick={() =>
+                    props.goToPage(Number(searchParams.get("page")) + 1)
+                  }
+                >
+                  {Number(searchParams.get("page")) + 1}
+                </Pagination.Item>
+                <Pagination.Next
+                  onClick={() =>
+                    props.goToPage(Number(searchParams.get("page")) + 1)
+                  }
+                />
+                <Pagination.Last
+                  onClick={() =>
+                    props.goToPage(
+                      Math.ceil(props.total / props.itemsPerPage - 1)
+                    )
+                  }
+                />
+              </>
+            )}
+            {Number(searchParams.get("page")) + 1 >=
+              props.total / props.itemsPerPage && (
+              <>
+                <Pagination.Next disabled />
+                <Pagination.Last disabled />
+              </>
+            )}
+          </Pagination>
+        </Stack>
+      );
     }
   }
-}
+};
 
 interface LoadingPaginationIProps {
-  size?: "sm" | "lg",
+  size?: "sm" | "lg";
 }
 
 export const LoadingPagination = (props: LoadingPaginationIProps) => {
-  return(
-      <Stack direction={"horizontal"} gap={2} className={"justify-content-end"}>
-        <Pagination className={"mb-0"} size={props.size ? props.size : undefined}>
-          <Pagination.First disabled/>
-          <Pagination.Prev disabled/>
-          <Pagination.Next disabled/>
-          <Pagination.Last disabled/>
-        </Pagination>
-      </Stack>
-      );
-}
+  return (
+    <Stack direction={"horizontal"} gap={2} className={"justify-content-end"}>
+      <Pagination className={"mb-0"} size={props.size ? props.size : undefined}>
+        <Pagination.First disabled />
+        <Pagination.Prev disabled />
+        <Pagination.Next disabled />
+        <Pagination.Last disabled />
+      </Pagination>
+    </Stack>
+  );
+};
 
 export default AggiePagination;
